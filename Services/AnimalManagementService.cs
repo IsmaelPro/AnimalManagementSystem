007f8cdda3457c447a1919d5f37609ded44b5951
Ismael Pinheiro <Ismael_Pinheiro@jabil.com>
2023-05-16 15:20:11 -0300

    adicionada separação de camadas

---
+++ Services/AnimalManagementService.cs
@@ -0,0 +1,21 @@
+﻿using Factories;
+using Models;
+
+namespace Services
+{
+    public class AnimalManagementService
+    {
+        private readonly IAnimalFactory _animalFactory;
+
+        public AnimalManagementService(IAnimalFactory animalFactory)
+        {
+            _animalFactory = animalFactory;
+        }
+
+        public void CreateAndMakeSound(string animalType)
+        {
+            Animal animal = _animalFactory.CreateAnimal(animalType);
+            animal.MakeSound();
+        }
+    }
+}
